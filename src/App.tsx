317947c4366a20b21ee9d5c3c6e78a8daac4f18f
KIRYL KHOMCHANKA <dickdurex265@gmail.com>
2024-02-19 16:16:54 +0300

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className='mx-auto my-0 bg-black max-w-7xl'>
+      <Header />
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="font-bold text-center text-[30px] text-white py-10">
+      15 puzzle
+    </header>
+  )
+}
+
+export default Header
