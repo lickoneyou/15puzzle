5e1a1865dba5c392df4b38a2b5246a4c555c36c8
KIRYL KHOMCHANKA <dickdurex265@gmail.com>
2024-02-20 18:12:19 +0300

    refactor handleClick

--- src/components/Puzzle.tsx
+++ src/components/Puzzle.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { handleClick } from '../handlers/handleClick'
 
 const Puzzle = () => {
   const [puzzle, setPuzzle] = useState([
@@ -10,52 +11,6 @@ const Puzzle = () => {
 
   const puzzleStyles =
     'border-2 text-white w-100 h-100 flex items-center justify-center box-border m-1 text-[20px] cursor-pointer'
-
-  type numPos = number | null
-
-  const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    let elementPosition: number[] = [0, 0]
-    let nullPosition: number[] = [0, 0]
-    let collision: numPos[] = []
-    const element: number = Number(e.currentTarget.textContent)
-    puzzle.forEach((el, i) => {
-      if (el.includes(element)) {
-        elementPosition = [i, el.indexOf(element)]
-        const positionTop = [i - 1, el.indexOf(element)]
-        const positionBottom = [i + 1, el.indexOf(element)]
-        const positionLeft = [i, el.indexOf(element) - 1]
-        const positionRight = [i, el.indexOf(element) + 1]
-        const top: numPos =
-          positionTop[0] >= 0 && positionTop[1] >= 0
-            ? puzzle[positionTop[0]][positionTop[1]]
-            : null
-        const bottom: numPos =
-          positionBottom[0] < puzzle.length && positionBottom[1] < puzzle.length
-            ? puzzle[positionBottom[0]][positionBottom[1]]
-            : null
-        const left: numPos =
-          positionLeft[0] >= 0 && positionLeft[1] >= 0
-            ? puzzle[positionLeft[0]][positionLeft[1]]
-            : null
-        const right: numPos =
-          positionRight[0] < el.length && positionRight[1] < el.length
-            ? puzzle[positionRight[0]][positionRight[1]]
-            : null
-
-        collision = [top, bottom, left, right]
-      }
-      if (el.includes(16)) {
-        nullPosition = [i, el.indexOf(16)]
-      }
-    })
-    if (collision.includes(16)) {
-      setPuzzle((state) => {
-        state[nullPosition[0]][nullPosition[1]] = Number(element)
-        state[elementPosition[0]][elementPosition[1]] = 16
-        return [...state]
-      })
-    }
-  }
 
   return (
     <>
@@ -73,7 +28,7 @@ const Puzzle = () => {
               <div
                 key={index}
                 className={puzzleStyles}
-                onClick={(e) => handleClick(e)}
+                onClick={(e) => handleClick(e, puzzle, setPuzzle)}
               >
                 {elem}
               </div>
@@ -86,3 +41,4 @@ const Puzzle = () => {
 }
 
 export default Puzzle
+
---
+++ src/handlers/handleClick.tsx
@@ -0,0 +1,45 @@
+ type numPos = number | null
+
+ export const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>, puzzle: number[][], setPuzzle: (value: React.SetStateAction<number[][]>) => void) => {
+    let elementPosition: number[] = [0, 0]
+    let nullPosition: number[] = [0, 0]
+    let collision: numPos[] = []
+    const element: number = Number(e.currentTarget.textContent)
+    puzzle.forEach((el, i) => {
+      if (el.includes(element)) {
+        elementPosition = [i, el.indexOf(element)]
+        const positionTop = [i - 1, el.indexOf(element)]
+        const positionBottom = [i + 1, el.indexOf(element)]
+        const positionLeft = [i, el.indexOf(element) - 1]
+        const positionRight = [i, el.indexOf(element) + 1]
+        const top: numPos =
+          positionTop[0] >= 0 && positionTop[1] >= 0
+            ? puzzle[positionTop[0]][positionTop[1]]
+            : null
+        const bottom: numPos =
+          positionBottom[0] < puzzle.length && positionBottom[1] < puzzle.length
+            ? puzzle[positionBottom[0]][positionBottom[1]]
+            : null
+        const left: numPos =
+          positionLeft[0] >= 0 && positionLeft[1] >= 0
+            ? puzzle[positionLeft[0]][positionLeft[1]]
+            : null
+        const right: numPos =
+          positionRight[0] < el.length && positionRight[1] < el.length
+            ? puzzle[positionRight[0]][positionRight[1]]
+            : null
+
+        collision = [top, bottom, left, right]
+      }
+      if (el.includes(16)) {
+        nullPosition = [i, el.indexOf(16)]
+      }
+    })
+    if (collision.includes(16)) {
+      setPuzzle((state: number[][]) => {
+        state[nullPosition[0]][nullPosition[1]] = Number(element)
+        state[elementPosition[0]][elementPosition[1]] = 16
+        return [...state]
+      })
+    }
+  }
