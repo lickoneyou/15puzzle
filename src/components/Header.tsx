a723222c66ad6c1379d002d2403ea23f6c1fb3ff
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-02-28 12:36:39 +0300

    fix select header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <header className="font-bold text-center text-[30px] text-white py-10 uppercase">
+    <header className="font-bold text-center text-[30px] text-white py-10 uppercase select-none">
       15 puzzle
     </header>
   )
