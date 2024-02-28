770d1d29e7ed6d42e8ca8709229e76f478ca6b14
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-02-28 18:10:16 +0300

    fix winnerNotification fn

--- src/handlers/winnerNotification.ts
+++ src/handlers/winnerNotification.ts
@@ -5,16 +5,23 @@ const winnerNotification = (arr: number[][], clicks: number) => {
   const winPosition = '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16'
   if (arr.flat().join() === winPosition) {
     const winners = checkWinners()
-    if (!winners[3] || winners[3].clicks > clicks) {
+    if (winners.length === 3 && winners[2].clicks > clicks) {
       const winnerName = prompt('You Win! Enter your name:')
       winners.push({ name: winnerName, clicks: clicks + 1 })
-      const sortWinners = winners.sort((a: Iwinners, b: Iwinners) => a.clicks - b.clicks)
-      localStorage.setItem(
-        'winners',
-        JSON.stringify(
-          sortWinners,
-        ),
+      const sortWinners = winners.sort(
+        (a: Iwinners, b: Iwinners) => a.clicks - b.clicks,
       )
+      sortWinners.pop()
+      localStorage.setItem('winners', JSON.stringify(sortWinners))
+      return
+    }
+    if (winners.length < 3) {
+      const winnerName = prompt('You Win! Enter your name:')
+      winners.push({ name: winnerName, clicks: clicks + 1 })
+      const sortWinners = winners.sort(
+        (a: Iwinners, b: Iwinners) => a.clicks - b.clicks,
+      )
+      localStorage.setItem('winners', JSON.stringify(sortWinners))
     } else {
       alert('You Win!')
     }
