7ba78934348c2f323646f8b28bd5954edc61e3ed
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-02-28 18:51:52 +0300

    add score table

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -1,15 +1,29 @@
 import React from 'react'
+import { Iwinners } from '../interfaces/Iwinners'
 
 const Score = () => {
-const tableStyles = 'border-white border-b-4 text-white text-[25px] p-2'
+  const tableStyles = 'text-white text-[25px] p-2'
 
   return (
-    <div>
+    <div className="w-3/5 mx-auto">
       <h2 className="text-white text-[30px] text-center">Score</h2>
-      <div className='flex justify-center'>
+      <div className="flex justify-around border-white border-b-4">
         <div className={tableStyles}>Place</div>
         <div className={tableStyles}>Name</div>
         <div className={tableStyles}>Clicks</div>
+      </div>
+      <div className="flex flex-col">
+        {JSON.parse(localStorage.getItem('winners') as string).map(
+          (el: Iwinners, index: number) => {
+            return (
+              <div key={`${index}${el.name}${el.clicks}`} className="flex justify-around border-white border-b-4 p-2">
+                <div className={tableStyles}>{index + 1}</div>
+                <div className={tableStyles}>{el.name}</div>
+                <div className={tableStyles}>{el.clicks}</div>
+              </div>
+            )
+          },
+        )}
       </div>
     </div>
   )
