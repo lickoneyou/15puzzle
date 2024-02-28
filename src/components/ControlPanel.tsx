388b90de42ee08e625af7c7e7e7c781bd61b768e
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-02-28 13:23:44 +0300

    fix record btn

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -8,7 +8,7 @@ import { useDispatch } from 'react-redux'
 const ControlPanel = ({ puzzle, setPuzzle }: Props) => {
   const clicks = useSelector((state: IRootState) => state)
   const [btnText, setBtnText] = useState('Start')
-  const [scoreHref, setScoreHref] = useState('#score')
+  const [scoreHref, setScoreHref] = useState('#state')
   const dispatch = useDispatch()
   const btnStyles =
     'text-white text-[25px] border-2 py-2 px-4 hover:bg-white hover:text-black transition ease-in-out duration-150 select-none'
@@ -27,7 +27,7 @@ const ControlPanel = ({ puzzle, setPuzzle }: Props) => {
       <a
         href={scoreHref}
         onClick={() =>
-          setScoreHref((state) => (state === '#' ? '#score' : '#'))
+          setScoreHref((state) => (state.includes('score') ? '#' : '#score'))
         }
         className={[btnStyles, 'inline-block'].join(' ')}
       >
--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Score = () => {
   return (
-    <div>Score</div>
+    <div className='text-white'>Score</div>
   )
 }
 
