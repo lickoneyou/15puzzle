cfeada4a29b19d9cbfff6edd17482cacae19a407
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-02-28 12:36:14 +0300

    fix setect control panel

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -12,10 +12,12 @@ const ControlPanel = ({ puzzle, setPuzzle }: Props) => {
 
   return (
     <div>
-      <p className="text-white uppercase text-[25px]">clicks: {clicks}</p>
+      <p className="text-white uppercase text-[25px] select-none">
+        clicks: {clicks}
+      </p>
       <button
         onClick={() => startGame(puzzle, setPuzzle, setBtnText, dispatch)}
-        className="text-white text-[25px] border-2 py-2 px-4 hover:bg-white hover:text-black transition ease-in-out duration-150"
+        className="text-white text-[25px] border-2 py-2 px-4 hover:bg-white hover:text-black transition ease-in-out duration-150 select-none"
       >
         {btnText}
       </button>
