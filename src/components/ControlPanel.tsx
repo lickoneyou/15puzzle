94e6ea6f15b18d9d240e4fa875a2959d92f0ef57
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-02-28 18:59:26 +0300

    fix Records btn

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -8,7 +8,7 @@ import { useDispatch } from 'react-redux'
 const ControlPanel = ({ puzzle, setPuzzle }: Props) => {
   const clicks = useSelector((state: IRootState) => state)
   const [btnText, setBtnText] = useState('Start')
-  const [scoreHref, setScoreHref] = useState('#state')
+  const [scoreHref, setScoreHref] = useState(window.location.hash)
   const dispatch = useDispatch()
   const btnStyles =
     'text-white text-[25px] border-2 py-2 px-4 hover:bg-white hover:text-black transition ease-in-out duration-150 select-none'
@@ -26,9 +26,9 @@ const ControlPanel = ({ puzzle, setPuzzle }: Props) => {
       </button>
       <a
         href={scoreHref}
-        onClick={() =>
+        onClick={() => {
           setScoreHref((state) => (state.includes('score') ? '#' : '#score'))
-        }
+        }}
         className={[btnStyles, 'inline-block'].join(' ')}
       >
         Records
